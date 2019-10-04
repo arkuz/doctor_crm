fdfab6670a40cb5f1891b2cbd208b89583d8ece6
Artem Kuzmenko <arkuzka@mail.ru>
2019-10-04 16:37:03 +0300

    cleared config file

--- webapp/config.py
+++ webapp/config.py
@@ -5,10 +5,10 @@ basedir = os.path.abspath(os.path.dirname('__file__'))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'webapp.db')
 
 # ключ API сервиса https://apidata.mos.ru
-DATA_MOS_RU_API_KEY = '40efbeeb79728efe0bc580f6174da03d'
+DATA_MOS_RU_API_KEY = ''
 
 # соль для паролей пользователей
-PASSWORD_SALT = 'c8b4df885065976921d20716e4a1a36bb580d0e47076cfab9d421d51f32ee3dc'
+PASSWORD_SALT = ''
 
 # секретный ключ для сессии
-SESSION_SECRET_KEY = b'()*&%&087gho9h)'
+SESSION_SECRET_KEY = b''
