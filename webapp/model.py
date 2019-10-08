50b022d350f578adfd660485fc2be0a6c1ca5d49
Ilya Gusev <gusev.ilya.a@gmail.com>
2019-10-08 09:06:11 +0300

    Added specialist and patient to db model

--- webapp/model.py
+++ webapp/model.py
@@ -11,3 +11,24 @@ class Users(db.Model):
 
     def __repr__(self):
         return '<Users {0} {1}>'.format(self.title, self.url)
+
+
+class MedicalSpecialist(db.Model):
+    __tablename__ = 'specialist'
+    specialist_id = db.Column(db.Integer, primary_key=True)
+    specialist_surename = db.Column(db.String, nullable=True)
+    city = db.Column(db.String, nullable=True)
+    hospital = db.Column(db.String, nullable=True)
+    department = db.Column(db.String, nullable=True)
+    patient = db.relationship(
+        'Patient', backref='specialist')
+
+
+class Patient(db.Model):
+    __tablename__ = 'patient'
+    patient_id = db.Column(db.Integer, primary_key=True)
+    patient_surename = db.Column(db.String, nullable=True)
+    city = db.Column(db.String, nullable=True)
+    disease = db.Column(db.String, nullable=True)
+    specialist_id = db.Column(
+        db.Integer, db.ForeignKey('specialist.specialist_id'))
