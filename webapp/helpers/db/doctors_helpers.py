0640b6f200e41cc73cbab14a23f82088f50af676
Artem Kuzmenko <arkuzka@mail.ru>
2019-10-16 20:37:54 +0300

    updated doctor_helpers

--- webapp/helpers/db/doctors_helpers.py
+++ webapp/helpers/db/doctors_helpers.py
@@ -1,6 +1,4 @@
-import hashlib
 from webapp.model import db, Doctor
-from webapp.config import PASSWORD_SALT
 
 
 def is_user_exist(email):
@@ -10,20 +8,9 @@ def is_user_exist(email):
 def registration(email, password):
     reg_success = False
     if not is_user_exist(email):
-        user = Doctor(email=email,
-                      password=get_password_hash(password))
-        db.session.add(user)
+        doctor = Doctor(email=email)
+        doctor.set_password(password)
+        db.session.add(doctor)
         db.session.commit()
         reg_success = True
     return reg_success
-
-
-def login(email, password):
-    return Doctor.query.filter(Doctor.email == email).filter(Doctor.password == get_password_hash(password)).count()
-
-
-def get_password_hash(password):
-    password = password.strip()
-    password_hash = hashlib.sha512()
-    password_hash.update(f'{PASSWORD_SALT}{password}'.encode('utf-8'))
-    return password_hash.hexdigest()
