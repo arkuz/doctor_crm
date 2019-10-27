0623a287179c604bc04eb2fa74bd93c24b761c01
Ilya Gusev <gusev.ilya.a@gmail.com>
2019-10-27 14:47:41 +0300

    добавлена страница администратора в возможностью редактировать модель Doctor

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from flask_login import LoginManager
-
 from webapp.db import db
 from webapp.doctor.models import Doctor
 from webapp.patient.models import Patient
@@ -9,7 +10,6 @@ from webapp.case.models import Case
 from webapp.doctor.views import blueprint as doctor_blueprint
 from webapp.patient.views import blueprint as patient_blueprint
 from webapp.case.views import blueprint as case_blueprint
-from webapp.admin.views import blueprint as admin_blueprint
 from webapp.clinic.views import blueprint as clinic_blueprint
 from webapp.api.views import blueprint as api_blueprint
 
@@ -19,6 +19,8 @@ def create_app():
     app.config.from_pyfile('config.py')
     app.secret_key = app.config['SESSION_SECRET_KEY']
     db.init_app(app)
+    admin = Admin(app)
+    admin.add_view(ModelView(Doctor, db.session, endpoint='doctor_db'))
 
     login_manager = LoginManager()
     login_manager.init_app(app)
@@ -31,7 +33,6 @@ def create_app():
     app.register_blueprint(doctor_blueprint)
     app.register_blueprint(patient_blueprint)
     app.register_blueprint(case_blueprint)
-    app.register_blueprint(admin_blueprint)
     app.register_blueprint(clinic_blueprint)
     app.register_blueprint(api_blueprint)
 
