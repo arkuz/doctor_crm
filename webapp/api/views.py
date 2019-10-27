a5a56f86906955ee57d17870085dd654a8428007
Ilya Gusev <gusev.ilya.a@gmail.com>
2019-10-27 11:33:14 +0300

    проблема с вызовом flask-admin

--- webapp/api/views.py
+++ webapp/api/views.py
@@ -1,6 +1,8 @@
-from flask import jsonify
+from flask import jsonify, current_app
 from flask_login import current_user
 from flask import Blueprint
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 from webapp.doctor.models import Doctor
 
@@ -21,4 +23,14 @@ def get_doctor_by_id(doctor_id):
     if current_user.is_authenticated:
         doctor = [doc.json_dump() for doc in Doctor.query.filter(Doctor.id == doctor_id)]
         return jsonify({'doctors': doctor})
+    return 'Ошибка, вы не авторизованы!'
+
+
+@blueprint.route('/db_view')
+def db_view():
+    if current_user.is_authenticated:
+        admin_app = Admin(current_app)
+        db = current_app
+        admin_view = admin_app.add_view(ModelView(Doctor, db.session))
+        return admin_view
     return 'Ошибка, вы не авторизованы!'
